da5e0b15b75280ff970a22de99c8c7095f86cac7
Cristian Marcel <lcristianiim@yahoo.com>
2015-12-28 18:57:56 +0200

    Correct configuration path

--- config.rb
+++ config.rb
@@ -3,8 +3,8 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "/"
-sass_dir = "/"
+css_dir = "../../public/css"
+sass_dir = "sass"
 images_dir = "images"
 
 # You can select your preferred output style here (can be overridden via the command line):
