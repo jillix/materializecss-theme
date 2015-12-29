0a853fc261a00f3061e55e07d5a98a6593f7c2f7
Cristian Marcel <lcristianiim@yahoo.com>
2015-12-29 22:12:18 +0200

    Css path

--- config.rb
+++ config.rb
@@ -3,7 +3,7 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "../../public/css"
+css_dir = "../../public/stylesheets/css"
 sass_dir = "sass"
 images_dir = "images"
 
